8f5620c016e4869dabae6e660a36331f11b412b0
Treece <Pashweetie@gmail.com>
2014-01-28 09:07:10 -0700

    nearly finished i just have some run-time errors

--- GridWorldCode/projects/critters/ChameleonKid.java
+++ GridWorldCode/projects/critters/ChameleonKid.java
@@ -18,6 +18,7 @@
 
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
+import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
 import java.util.ArrayList;
@@ -29,7 +30,7 @@ import java.awt.Color;
  * it moves through the grid. <br />
  * The implementation of this class is testable on the AP CS A and AB exams.
  */
-public class ChameleonCritter extends Critter
+public class ChameleonKid extends ChameleonCritter
 {
 
     private static final double DARKENING_FACTOR = 0.05;
@@ -37,31 +38,34 @@ public class ChameleonCritter extends Critter
      * Randomly selects a neighbor and changes this critter's color to be the
      * same as that neighbor's. If there are no neighbors, no action is taken.
      */
-    public void processActors(ArrayList<Actor> actors)
+    public ArrayList<Actor> getActors()
     {
-        int n = actors.size();
-        if (n == 0){
-            Color c = getColor();
-            int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
-            int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
-            int blue = (int) (c.getBlue() * (1 - DARKENING_FACTOR));
-
-            setColor(new Color(red, green, blue));
+        ArrayList<Actor> actors = new ArrayList<Actor>();
+        int[] dirs =
+            { Location.AHEAD, Location.HALF_CIRCLE};
+        for (Location loc : getLocationsInDirections(dirs))
+        {
+            Actor a = getGrid().get(loc);
+            if (a != null)
+                actors.add(a);
         }
-            
-        int r = (int) (Math.random() * n);
 
-        Actor other = actors.get(r);
-        setColor(other.getColor());
+        return actors;
     }
 
-    /**
-     * Turns towards the new location as it moves.
-     */
-    public void makeMove(Location loc)
+    
+    public ArrayList<Location> getLocationsInDirections(int[] directions)
     {
-        setDirection(getLocation().getDirectionToward(loc));
-        
-        super.makeMove(loc);
-    }
+        ArrayList<Location> locs = new ArrayList<Location>();
+        Grid gr = getGrid();
+        Location loc = getLocation();
+    
+        for (int d : directions)
+        {
+            Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+            if (gr.isValid(neighborLoc))
+                locs.add(neighborLoc);
+        }
+        return locs;
+    } 
 }
